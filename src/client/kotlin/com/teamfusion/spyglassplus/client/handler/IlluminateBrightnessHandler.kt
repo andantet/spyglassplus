063c48c647a047a734e73d27431e9774228df307
Daniel Orr <orrdaniel73@gmail.com>
2024-03-08 11:04:39 +0000

    Make illuminate more dynamic

--- src/client/kotlin/com/teamfusion/spyglassplus/client/handler/IlluminateBrightnessHandler.kt
+++ src/client/kotlin/com/teamfusion/spyglassplus/client/handler/IlluminateBrightnessHandler.kt
@@ -8,31 +8,46 @@ import com.teamfusion.spyglassplus.event.TypedEventResult
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.network.ClientPlayerEntity
 import net.minecraft.enchantment.EnchantmentHelper
-import net.minecraft.util.Util
+import net.minecraft.item.ItemStack
 import kotlin.math.max
 import kotlin.math.min
 
+/**
+ * Handles world brightness for the illuminate enchantment.
+ */
 class IlluminateBrightnessHandler : ClientWorldBrightnessCallback {
-    private var openedLastTick = false
-    private var millisOpened: Long = -1
+    /**
+     * When the spyglass opened.
+     */
+    private var progress: Float = 0.0f
 
     override fun getBrightness(client: MinecraftClient, player: ClientPlayerEntity, originalValue: Float): TypedEventResult<Float> {
-        val stack = getActiveSpyglassItemStack(client, player)
-        if (stack != null) {
-            val level = EnchantmentHelper.getLevel(SpyglassPlusEnchantments.ILLUMINATE, stack)
-            if (level > 0) {
-                val ms = Util.getMeasuringTimeMs()
-                if (!openedLastTick) {
-                    millisOpened = ms
-                    openedLastTick = true
-                }
+        // calculate progress delta
+        val delta = 0.0005f * client.tickDelta
 
-                val diff = ms - millisOpened
-                return TypedEventResult(EventResult.CANCEL, min(1.0f, max(originalValue, diff / 500.0f)))
-            }
+        // check stack for illuminate
+        val stack = getActiveSpyglassItemStack(client, player)
+        if (hasIlluminate(stack)) {
+            progress += delta
+        } else {
+            progress -= delta * 3
         }
 
-        openedLastTick = false
+        // clamp progress
+        progress = min(1.0f, max(0.0f, progress))
+
+        // apply possible progress
+        if (stack != null) {
+            return TypedEventResult(EventResult.CANCEL, min(1.0f, max(originalValue, progress)))
+        }
+
         return TypedEventResult.pass()
+    }
+
+    /**
+     * Checks whether a nullable stack has the illuminate enchantment.
+     */
+    private fun hasIlluminate(stack: ItemStack?): Boolean {
+        return stack != null && EnchantmentHelper.getLevel(SpyglassPlusEnchantments.ILLUMINATE, stack) > 0
     }
 }
